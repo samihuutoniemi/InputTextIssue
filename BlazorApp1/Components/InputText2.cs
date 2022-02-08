489e4c8db089a65cf8d5113d5a22899462572732
Sami Huutoniemi <sami.huutoniemi@gmail.com>
2022-02-08 11:00:50 +0100

    Add files via upload
    init

---
+++ BlazorApp1/Components/InputText2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorApp1.Components;
+
+public class InputText2 : InputText
+{
+    [Parameter]
+    public Func<string, string> Formatter { get; set; }
+
+    protected override bool TryParseValueFromString(string value, out string result, [NotNullWhen(false)] out string validationErrorMessage)
+    {
+        var parseResult = base.TryParseValueFromString(value, out result, out validationErrorMessage);
+
+        return parseResult;
+    }
+
+    protected override string FormatValueAsString(string value)
+    {
+        var result = Formatter?.Invoke(value) ?? value;
+        //CurrentValueAsString = result;
+
+        result = base.FormatValueAsString(result);
+
+        return result;
+    }
+}
